76fb0a4230be64723eda468c1ec1eca1ee3415b0
alber <969967764@qq.com>
2024-06-02 10:31:36 +0800

    项目初始化创建

---
+++ coupon/src/test/java/com/alber/gulimall/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alber.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
